fe5ea89d8f332781ae51bdce92051a5ba012a727
joshdpaul <jdpaul3@alaska.edu>
2024-11-15 13:28:42 -0900

    include pooled CI aggregation functions

--- utilities/luts.py
+++ utilities/luts.py
@@ -4,6 +4,9 @@ census_ = "839fc96162a9e16e7896434e7592eccaf7938706"
 # CDC app token
 cdc_ = "aBj1zOdJs530ivzSUlyOHkLaf"
 
+# use CDC app token?
+use_cdc_token = False
+
 var_dict = dict(
     {
         "dhc": {
